e36feb6638ad35a7361d5c3a01d1fc2319eb2e48
Henry-PC <henry9685@gmail.com>
2025-02-27 22:47:36 +0800

    Wrap as a class

--- app.py
+++ app.py
@@ -7,42 +7,47 @@ from linebot.models import MessageEvent, TextMessage, TextSendMessage
 from langdetect import detect
 import translators as ts
 
-# line token
-channel_access_token = os.environ['LINE_CHANNEL_ACCESS_TOKEN']
-channel_secret = os.environ['LINE_CHANNEL_SECRET']
-line_bot_api = LineBotApi(channel_access_token)
-handler = WebhookHandler(channel_secret)
 
-app = Flask(__name__)
+class LineBotHandler:
+  def __init__(self):
+    # line token
+    channel_access_token = os.environ['LINE_CHANNEL_ACCESS_TOKEN']
+    channel_secret = os.environ['LINE_CHANNEL_SECRET']
+    self.line_bot_api = LineBotApi(channel_access_token)
+    self.handler = WebhookHandler(channel_secret)
+    self.app = Flask(__name__)
+    self.lang_target = {'en': 'zh-Hant', 'zh-tw': 'en', 'zh-cn': 'en', 'ko': 'en', 'ja': 'en'}
 
-lang_target = {'en': 'zh-Hant', 'zh-tw': 'en', 'zh-cn': 'en', 'ko': 'en', 'ja': 'en'}
+  def callback(self):
+    self.app.route("/callback", methods=['POST'])
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+    # get request body as text
+    body = request.get_data(as_text=True)
+    self.app.logger.info("Request body: " + body)
+    # handle webhook body
+    try:
+      self.handler.handle(body, signature)
+    except InvalidSignatureError:
+      abort(400)
+    return 'OK'
 
-
-@app.route("/callback", methods=['POST'])
-def callback():
-  # get X-Line-Signature header value
-  signature = request.headers['X-Line-Signature']
-  # get request body as text
-  body = request.get_data(as_text=True)
-  app.logger.info("Request body: " + body)
-  # handle webhook body
-  try:
-    handler.handle(body, signature)
-  except InvalidSignatureError:
-    abort(400)
-  return 'OK'
-
-
-@handler.add(MessageEvent, message=TextMessage)
-def handle_message(event: MessageEvent):
-  # translate
-  msg = event.message.text
-  ts_text = ts.translate_text(query_text=msg,
-                              to_language=lang_target.get(detect(msg), 'zh-Hant'))
-  message = TextSendMessage(text=ts_text)
-  line_bot_api.reply_message(event.reply_token, message)
+  def handle_message(self, event: MessageEvent):
+    self.handler.add(MessageEvent, message=TextMessage)
+    # translate
+    msg = str(event.message.text).strip()
+    if msg == 'Word of the day':
+      ts_text = 't1'
+    elif msg == 'Phrase of the day':
+      ts_text = 't2'
+    else:
+      ts_text = ts.translate_text(query_text=msg,
+                                  to_language=self.lang_target.get(detect(msg), 'zh-Hant'))
+    message = TextSendMessage(text=ts_text)
+    self.line_bot_api.reply_message(event.reply_token, message)
 
 
 if __name__ == "__main__":
   port = int(os.environ.get('PORT', 5000))
-  app.run(host='0.0.0.0', port=port)
+  line_bot = LineBotHandler()
+  line_bot.app.run(host='0.0.0.0', port=port)
